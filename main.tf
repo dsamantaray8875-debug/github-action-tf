f21ba34bc4c3dadc2d3b4f24dae95c41df732aba
Deepak Kumar Samantaray <dsamantaray8875@gmail.com>
2026-08-04 00:13:30 +0530

    Initial commit

---
+++ main.tf
@@ -0,0 +1,15 @@
+resource "aws_vpc" "name" {
+    cidr_block = "10.0.0.0/16"
+    tags = {
+        Name = "test-vpc"
+    }
+  
+}
+
+resource "aws_subnet" "name2" {
+    vpc_id = aws_vpc.name.id
+    cidr_block = "10.0.1.0/24"
+    tags = {
+        Name = "test-subnet"
+    }
+}
